189f0eb5186df2d367dbf877e17a6e05dbadac34
Thibaut VARÈNE <hacks@slashdirt.org>
2016-09-21 22:30:09 +0000

    Add documentation Accept floats in delta_to_temp()

--- rwchcd_lib.h
+++ rwchcd_lib.h
@@ -14,17 +14,32 @@
 short validate_temp(const temp_t temp);
 temp_t get_temp(const tempid_t id);
 
+/**
+ * Convert celsius value to internal temp_t format (Kelvin *100).
+ * @param celsius temp value in Celsius
+ * @return value converted to internal type
+ */
 static inline temp_t celsius_to_temp(const float celsius)
 {
 	return ((temp_t)((celsius + 273.15F)*100));
 }
 
+/**
+ * Convert temperature from internal format to Celsius value.
+ * @param temp temp value as temp_t
+ * @return value converted to Celsius
+ */
 static inline float temp_to_celsius(const temp_t temp)
 {
 	return ((float)((float)temp/100.0F - 273.15F));
 }
 
-static inline temp_t delta_to_temp(const temp_t delta)
+/**
+ * Convert a temperature delta (in Celsius or Kelvin) to internal type.
+ * @param delta the delta value to be converted
+ * @return the corresponding value expressed in internal temperature format.
+ */
+static inline temp_t delta_to_temp(const float delta)
 {
 	return ((temp_t)(delta * 100));
 }
