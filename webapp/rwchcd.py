8837e6d965f39cd0106e76580c95b42f4ca49486
Thibaut VARÈNE <hacks@slashdirt.org>
2016-11-18 19:17:07 +0000

    update webapp to control temp

--- webapp/rwchcd.py
+++ webapp/rwchcd.py
@@ -17,21 +17,31 @@ urls = (
 )
 
 formMode = form.Form(
-	form.Dropdown('sysmode', [(0, 'Off'), (2, 'Confort'), (3, 'Eco'), (4, 'Hors-Gel'), (5, 'ECS')])
+	form.Dropdown('sysmode', [(0, 'Off'), (2, 'Confort'), (3, 'Eco'), (4, 'Hors-Gel'), (5, 'ECS')]),
+	form.Textbox('systemp', form.regexp('^\d+\.?\d*$', 'decimal number: xx.x')),
 	)
 
 class rwchcd:
 	def GET(self):
-		temp = "{:4.1f}".format(rwchcd_Control.ToutdoorGet())
+		outtemp = "{:4.1f}".format(rwchcd_Control.ToutdoorGet())
 		currmode = rwchcd_Control.SysmodeGet()
+		currtemp = "{:4.1f}".format(rwchcd_Control.ConfigTempGet())
 		fm = formMode()
 		fm.sysmode.value = currmode
-		return render.rwchcd(fm, temp)
+		fm.systemp.value = currtemp
+		return render.rwchcd(fm, outtemp)
 	def POST(self):
-		i = web.input()
-		mode = int(i['sysmode'])
-		rwchcd_Control.SysmodeSet(mode)
-		raise web.found(web.url())
+		temp = 0
+		form = formMode()
+		if not form.validates():
+			form.sysmode.value = int(form.sysmode.value)
+			return render.rwchcd(form, temp)
+		else:
+			mode = int(form.sysmode.value)
+			rwchcd_Control.SysmodeSet(mode)
+			newtemp = float(form.systemp.value)
+			rwchcd_Control.ConfigTempSet(systemp)
+			raise web.found(web.url())
 		
 
 if __name__ == "__main__":
