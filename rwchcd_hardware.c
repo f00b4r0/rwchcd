da788613fde9e2c61adee6b48fe4e3f78aeb0204
Thibaut VARÈNE <hacks@slashdirt.org>
2017-04-20 20:32:17 +0200

    split pt1000_ohm_to_celsius() and the quadratic solution for CVD

--- rwchcd_hardware.c
+++ rwchcd_hardware.c
@@ -148,30 +148,46 @@ static unsigned int sensor_to_ohm(const rwchc_sensor_t raw, const bool calib)
 // http://www.mosaic-industries.com/embedded-systems/microcontroller-projects/temperature-measurement/platinum-rtd-sensors/resistance-calibration-table
 
 /**
- * Convert Pt1000 resistance value to actual temperature.
+ * Convert resistance value to actual temperature based on Callendar - Van Dusen.
  * Use a quadratic fit for simplicity.
+ * http://aviatechno.net/thermo/rtd03.php
+ * https://www.newport.com/medias/sys_master/images/images/h4b/h16/8797291446302/TN-RTD-1-Callendar-Van-Dusen-Equation-and-RTD-Temperature-Sensors.pdf
+ * Rt = R0 + R0*alpha*[t - delta*(t/100 - 1)*(t/100) - beta*(t/100 - 1)*(t/100)^3]
+ * alpha is the mean R change referred to 0C
+ * Rt = R0 * [1 + A*t + B*t^2 - C*(t-100)*t^3]
+ * A = alpha + (alpha*delta)/100
+ * B = - (alpha * delta)/(100^2)
+ * C = - (alpha * beta)/(100^4)
+ * @param R0 nominal resistance at 0C
+ * @param A precomputed A parameter
+ * @param B precomputed B parameter
+ * @param ohm the resistance value to convert
+ * @return temperature in Celsius
+ */
+static float quadratic_cvd(const float R0, const float A, const float B, const uint_fast16_t ohm)
+{
+	// quadratic fit: we're going to ignore the cubic term given the temperature range we're looking at
+	return ((-R0*A + sqrtf(R0*R0*A*A - 4.0F*R0*B*(R0 - ohm))) / (2.0F*R0*B));
+}
+
+/**
+ * Convert Pt1000 resistance value to actual temperature.
+ * Use European Standard values.
  * @param ohm the resistance value to convert
  * @return temperature in Celsius
  */
 static float pt1000_ohm_to_celsius(const uint_fast16_t ohm)
 {
 	const float R0 = 1000.0F;
-	float alpha, delta, A, B, temp;
-
-	// manufacturer parameters
-	alpha = 0.003850F;	// mean R change referred to 0C
-	//beta = 0.10863F;
-	delta = 1.4999F;
+	const float alpha = 0.003850F;
+	const float delta = 1.4999F;
 
 	// Callendar - Van Dusen parameters
-	A = alpha + (alpha * delta) / 100;
-	B = (-alpha * delta) / (100 * 100);
+	const float A = alpha + (alpha * delta) / 100;
+	const float B = (-alpha * delta) / (100 * 100);
 	//C = (-alpha * beta) / (100 * 100 * 100 * 100);	// only for t < 0
 
-	// quadratic fit: we're going to ignore the cubic term given the temperature range we're looking at
-	temp = (-R0*A + sqrtf(R0*R0*A*A - 4*R0*B*(R0 - ohm))) / (2*R0*B);
-
-	return (temp);
+	return (quadratic_cvd(R0, A, B, ohm));
 }
 
 /**
