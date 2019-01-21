accb9751dd7c976641d8fe156c87e41ce17af29d
Thibaut VARÈNE <hacks@slashdirt.org>
2019-01-21 21:21:11 +0100

    bug^Wfeature

--- filecfg_parser.c
+++ filecfg_parser.c
@@ -113,7 +113,7 @@ int filecfg_parser_match_node(const struct s_filecfg_parser_node * const node, s
 		return (-EINVALID);
 
 	for (i = 0; i < nparsers; i++) {
-		if (parsers[i].type != node->type)
+		if (parsers[i].type != node->type)	// XXX this will enforce floats where ints are input. Possible fix via bitfield instead of enum
 			continue;	// skip invalid node type
 
 		if (!strcmp(parsers[i].identifier, node->name)) {
