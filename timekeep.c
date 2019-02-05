dce64513bc8bf586f8fe9159f561e4ccf96eafa0
Thibaut VARÈNE <hacks@slashdirt.org>
2019-02-05 20:39:45 +0100

    Add timekeeping routines

---
+++ timekeep.c
@@ -0,0 +1,92 @@
+//
+//  timekeep.c
+//  rwchcd
+//
+//  (C) 2019 Thibaut VARENE
+//  License: GPLv2 - http://www.gnu.org/licenses/gpl-2.0.html
+//
+
+/**
+ * @file
+ * Timekeeping implementation.
+ */
+
+#include <time.h>
+#include <unistd.h>	// sleep()
+
+#include "rwchcd.h"
+#include "timekeep.h"
+
+#define TIMEKEEP_RESNS	(1000000000L/TIMEKEEP_SMULT)	///< minimum clock source resolution required in nanoseconds.
+
+static clockid_t TK_clockid;				///< after initialization, holds the correct clock id.
+static struct timespec TK_tstart;			///< initial timestamp (set during initialization)
+
+/**
+ * Init timekeeping subsystem.
+ * This function tries various monotonic clock sources and will fail
+ * if none is available.
+ * @return exec status
+ */
+int timekeep_init(void)
+{
+	struct timespec res;
+	int ret;
+
+#ifdef CLOCK_MONOTONIC_COARSE
+	// test coarse
+	TK_clockid = CLOCK_MONOTONIC_COARSE;
+	ret = clock_getres(TK_clockid, &res);
+	if (!ret) {
+		if (!res.tv_sec && (TIMEKEEP_RESNS >= res.tv_nsec)) {	// all good
+			clock_gettime(TK_clockid, &TK_tstart);
+			return (ALL_OK);
+		}
+	}
+#endif
+
+	// didnt work or not available, test regular
+	TK_clockid = CLOCK_MONOTONIC;
+	ret = clock_getres(TK_clockid, &res);
+	if (!ret) {
+		if (!res.tv_sec && (TIMEKEEP_RESNS > res.tv_nsec)) {	// all good
+			clock_gettime(TK_clockid, &TK_tstart);
+			return (ALL_OK);
+		}
+	}
+
+	// nothing good so far, fail
+	return (-EINIT);
+}
+
+void timekeep_exit()
+{
+}
+
+/**
+ * Get the current timestamp.
+ * @return a monotonically growing timestamp value, with 0 being init time.
+ * @note wraparound is not handled (should happen after a few centuries uptime).
+ */
+timekeep_t timekeep_now(void)
+{
+	struct timespec tsnow;
+	timekeep_t retval;
+
+	if (clock_gettime(TK_clockid, &tsnow))
+		return (-1);
+
+	retval = (tsnow.tv_sec - TK_tstart.tv_sec) * TIMEKEEP_SMULT;
+	retval += (tsnow.tv_nsec - TK_tstart.tv_nsec) / TIMEKEEP_RESNS;
+
+	return (retval);
+}
+
+/**
+ * Sleep for at least N seconds.
+ * @param seconds time to sleep for.
+ */
+void timekeep_sleep(unsigned int seconds)
+{
+	while ((seconds = sleep(seconds)));
+}
---
+++ timekeep.h
@@ -0,0 +1,50 @@
+//
+//  timekeep.h
+//  rwchcd
+//
+//  (C) 2019 Thibaut VARENE
+//  License: GPLv2 - http://www.gnu.org/licenses/gpl-2.0.html
+//
+
+/**
+ * @file
+ * Timekeeping API.
+ */
+
+#ifndef timekeep_h
+#define timekeep_h
+
+#include <inttypes.h>
+
+#define TIMEKEEP_SMULT	10L				///< second multiplier: 10 -> 0.1s precision
+
+/** Timestamp 'tick' type. Timestamp precision is 0.1s (see TIMEKEEP_SMULT) */
+typedef int64_t	timekeep_t;
+
+int timekeep_init(void);
+void timekeep_exit(void);
+timekeep_t timekeep_now(void);
+void timekeep_sleep(unsigned int seconds);
+
+/**
+ * Convert seconds to timekeep_t format.
+ * @param seconds value to convert.
+ * @return the value correctly formatted.
+ */
+__attribute__((const, always_inline)) static inline timekeep_t timekeep_sec_to_tk(long long seconds)
+{
+	return (seconds * TIMEKEEP_SMULT);
+}
+
+
+/**
+ * Convert timekeep_t format back to seconds.
+ * @param tk value to convert.
+ * @return the value expressed in seconds.
+ */
+__attribute__((const, always_inline)) static inline long timekeep_tk_to_sec(timekeep_t tk)
+{
+	return (tk / TIMEKEEP_SMULT);
+}
+
+#endif /* timekeep_h */
