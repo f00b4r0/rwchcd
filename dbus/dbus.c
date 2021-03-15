276f819e6b581e47a18189282044b81dc55a484e
Thibaut VARÈNE <hacks@slashdirt.org>
2021-03-15 20:07:56 +0100

    dbus: rewrite, support RW runtime system/run/dhw-modes

--- dbus/dbus.c
+++ dbus/dbus.c
@@ -2,7 +2,7 @@
 //  dbus.c
 //  rwchcd
 //
-//  (C) 2016-2018 Thibaut VARENE
+//  (C) 2016-2018,2021 Thibaut VARENE
 //  License: GPLv2 - http://www.gnu.org/licenses/gpl-2.0.html
 //
 
@@ -20,257 +20,146 @@
  * Heat source / circuit / dhwt). See NetworkManager?
  */
 
+#include <gio/gio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include "lib.h"
 #include "runtime.h"
-#include "models.h"	// models_outtemp()
 #include "plant/plant.h"
 #include "dbus.h"
-#include "dbus-generated.h"
 
+#define DBUS_RUNTIME_IFACE "org.slashdirt.rwchcd.Runtime"
+#define DBUS_CIRCUIT_IFACE "org.slashdirt.rwchcd.Circuit"
+
+static GDBusNodeInfo *dbus_introspection_data = NULL;
+static GDBusInterfaceInfo *dbus_runtime_interface_info = NULL;
+static GDBusInterfaceInfo *dbus_circuit_interface_info = NULL;
+
+static const gchar dbus_introspection_xml[] =
+"<node>"
+" <interface name='" DBUS_RUNTIME_IFACE "'>"
+"  <property name='SystemMode' access='readwrite' type='y' />"
+"  <property name='RunMode' access='readwrite' type='y' />"
+"  <property name='DhwMode' access='readwrite' type='y' />"
+" </interface>"
+" <interface name='" DBUS_CIRCUIT_IFACE "'>"
+"  <property name='RunMode' access='readwrite' type='y' />"
+"  <property name='TempComfort' access='readwrite' type='d' />"
+"  <property name='TempEco' access='readwrite' type='d' />"
+"  <property name='TempFrostFree' access='readwrite' type='d' />"
+"  <property name='OutOffComfort' access='readwrite' type='d' />"
+"  <property name='OutOffEco' access='readwrite' type='d' />"
+"  <property name='OutOffFrostFree' access='readwrite' type='d' />"
+" </interface>"
+"</node>";
 
 static GMainLoop *Mainloop = NULL;
 
-/**
- * D-Bus method ToutdoorGet handler.
- * Replies with current outdoor temperature.
- * @bug we don't lock because we don't care
- */
-static gboolean on_handle_toutdoor_get(dbusRwchcdControl *object,
-				      GDBusMethodInvocation *invocation,
-				      __attribute__((unused)) gpointer user_data)
+static GVariant *
+runtime_get_property(GDBusConnection  *connection,
+		    const gchar      *sender,
+		    const gchar      *object_path,
+		    const gchar      *interface_name,
+		    const gchar      *property_name,
+		    GError          **error,
+		    gpointer          user_data)
 {
-	float temp = temp_to_celsius(models_outtemp());
-	
-	dbus_rwchcd_control_complete_toutdoor_get(object, invocation, temp);
-	
-	return TRUE;
-}
+	GVariant *var;
+	const gchar *node;
 
-/**
- * D-Bus method SysmodeGet handler.
- * Replies with current system mode.
- */
-static gboolean on_handle_sysmode_get(dbusRwchcdControl *object,
-				      GDBusMethodInvocation *invocation,
-				      __attribute__((unused)) gpointer user_data)
-{
-	enum e_systemmode cursys = runtime_systemmode();
-
-	dbus_rwchcd_control_complete_sysmode_get(object, invocation, (guchar)cursys);
-	
-	return TRUE;
-}
-
-/**
- * D-Bus method SystemSet handler.
- * Sets the desired system mode.
- * @param Sysmode Target system mode
- */
-static gboolean on_handle_sysmode_set(dbusRwchcdControl *object,
-				      GDBusMethodInvocation *invocation,
-				      guchar Sysmode,
-				      __attribute__((unused)) gpointer user_data)
-{
-	enum e_systemmode newsysmode;
-	
-	if ((Sysmode > SYS_NONE) && (Sysmode < SYS_UNKNOWN))
-		newsysmode = Sysmode;
+	var = NULL;
+	if (g_strcmp0(property_name, "SystemMode") == 0) {
+		enum e_systemmode sysmode = runtime_systemmode();
+		var = g_variant_new_byte((guchar)sysmode);
+	}
+	else if (g_strcmp0(property_name, "RunMode") == 0) {
+		enum e_runmode runmode = runtime_runmode();
+		var = g_variant_new_byte((guchar)runmode);
+	}
+	else if (g_strcmp0(property_name, "DhwMode") == 0) {
+		enum e_runmode dhwmode = runtime_dhwmode();
+		var = g_variant_new_byte((guchar)dhwmode);
+	}
 	else
-		return FALSE;
+		g_assert_not_reached();
 
-	runtime_set_systemmode(newsysmode);
-	
-	dbus_rwchcd_control_complete_sysmode_set(object, invocation);
-	
+	return var;
+}
+
+static gboolean
+runtime_set_property(GDBusConnection  *connection,
+		    const gchar      *sender,
+		    const gchar      *object_path,
+		    const gchar      *interface_name,
+		    const gchar      *property_name,
+		    GVariant         *value,
+		    GError          **error,
+		    gpointer          user_data)
+{
+	if (g_strcmp0(property_name, "SystemMode") == 0) {
+		enum e_systemmode sysmode = g_variant_get_byte(value);
+		if ((sysmode > SYS_NONE) && (sysmode < SYS_UNKNOWN))
+			runtime_set_systemmode(sysmode);
+		else
+			goto error;
+	}
+	else if (g_strcmp0(property_name, "RunMode") == 0) {
+		enum e_runmode runmode = g_variant_get_byte(value);
+		if ((runmode >= 0) && (runmode < RM_UNKNOWN))
+			runtime_set_runmode(runmode);
+		else
+			goto error;
+	}
+	else if (g_strcmp0(property_name, "DhwMode") == 0) {
+		enum e_runmode runmode = g_variant_get_byte(value);
+		if ((runmode >= 0) && (runmode < RM_UNKNOWN))
+			runtime_set_dhwmode(runmode);
+		else
+			goto error;
+	}
+	else
+		g_assert_not_reached();
+
 	return TRUE;
+
+error:
+	g_set_error_literal(error,
+		    G_IO_ERROR,
+		    G_IO_ERROR_FAILED,
+		    "Invalid argument");
+	return FALSE;
 }
 
-/**
- * D-Bus method ConfigTempModeGet handler.
- * Replies with current config default circuit temperature for the given runmode. XXX QUICK HACK.
- * @param Runmode target runmode for query
- * @note only handles default circuit temp for now.
- * @todo make it generic to get any config temp
- */
-static gboolean on_handle_config_temp_mode_get(dbusRwchcdControl *object,
-					       GDBusMethodInvocation *invocation,
-					       guchar Runmode,
-					       __attribute__((unused)) gpointer user_data)
+static const GDBusInterfaceVTable runtime_vtable = {
+	NULL,
+	runtime_get_property,
+	runtime_set_property,
+};
+
+static void on_bus_acquired(GDBusConnection *connection, const gchar *name, gpointer user_data)
 {
-	struct s_runtime * restrict const runtime = runtime_get();
-	enum e_runmode runmode = Runmode;
-	temp_t systemp;
-	float temp;
-	bool err = false;
-	
-	pthread_rwlock_rdlock(&runtime->runtime_rwlock);
-	switch (runmode) {
-		case RM_COMFORT:
-			systemp = runtime->plant->pdata.set.def_hcircuit.t_comfort;
-			break;
-		case RM_ECO:
-			systemp = runtime->plant->pdata.set.def_hcircuit.t_eco;
-			break;
-		case RM_FROSTFREE:
-			systemp = runtime->plant->pdata.set.def_hcircuit.t_frostfree;
-			break;
-		default:
-			err = true;
-			break;
-	}
-	pthread_rwlock_unlock(&runtime->runtime_rwlock);
-	
-	if (err)
-		return false;
-	
-	temp = temp_to_celsius(systemp);
-	
-	dbus_rwchcd_control_complete_config_temp_mode_get(object, invocation, temp);
-	
-	return true;
-}
+	guint registration_id;
 
-/**
- * D-Bus method ConfigTempModeSet handler.
- * Sets the desired config default circuit temperature for the given runmode.
- * @param Runmode target runmode for the new temp
- * @param NewTemp new temperature value
- * @note only handles default circuit temp for now.
- * @warning doesn't save runtime after set
- * @deprecated XXX QUICK HACK incompatible with file-based configuration.
- */
-__attribute__ ((deprecated))
-static gboolean on_handle_config_temp_mode_set(dbusRwchcdControl *object,
-					       GDBusMethodInvocation *invocation,
-					       guchar Runmode,
-					       gdouble NewTemp,
-					       __attribute__((unused)) gpointer user_data)
-{
-	struct s_runtime * restrict const runtime = runtime_get();
-	temp_t newtemp = celsius_to_temp(NewTemp);
-	enum e_runmode runmode = Runmode;
-	bool err = false;
+	registration_id = g_dbus_connection_register_object(connection,
+							    "/org/slashdirt/rwchcd",
+							    dbus_runtime_interface_info,
+							    &runtime_vtable,
+							    NULL,  /* user_data */
+							    NULL,  /* user_data_free_func */
+							    NULL); /* GError** */
+	g_assert (registration_id > 0);
 
-	if (validate_temp(newtemp) != ALL_OK)
-		return false;
-
-	pthread_rwlock_wrlock(&runtime->runtime_rwlock);
-	switch (runmode) {
-		case RM_COMFORT:
-			runtime->plant->pdata.set.def_hcircuit.t_comfort = newtemp;
-			break;
-		case RM_ECO:
-			runtime->plant->pdata.set.def_hcircuit.t_eco = newtemp;
-			break;
-		case RM_FROSTFREE:
-			runtime->plant->pdata.set.def_hcircuit.t_frostfree = newtemp;
-			break;
-		default:
-			err = true;
-			break;
-	}
-	pthread_rwlock_unlock(&runtime->runtime_rwlock);
-
-	if (err)
-		return false;
-	
-	dbus_rwchcd_control_complete_config_temp_mode_set(object, invocation);
-	
-	return true;
-}
-
-/**
- * D-Bus method ConfigOuthoffModeGet handler.
- * Replies with current config default circuit outhoff temp for the given runmode. XXX QUICK HACK.
- * @param Runmode target runmode for query
- * @note only handles default circuit outhoff temp for now.
- * @todo make it generic to set any config temp
- */
-static gboolean on_handle_config_outhoff_mode_get(dbusRwchcdControl *object,
-					       GDBusMethodInvocation *invocation,
-					       guchar Runmode,
-					       __attribute__((unused)) gpointer user_data)
-{
-	struct s_runtime * restrict const runtime = runtime_get();
-	enum e_runmode runmode = Runmode;
-	temp_t systemp;
-	float temp;
-	bool err = false;
-
-	pthread_rwlock_rdlock(&runtime->runtime_rwlock);
-	switch (runmode) {
-		case RM_COMFORT:
-			systemp = runtime->plant->pdata.set.def_hcircuit.outhoff_comfort;
-			break;
-		case RM_ECO:
-			systemp = runtime->plant->pdata.set.def_hcircuit.outhoff_eco;
-			break;
-		case RM_FROSTFREE:
-			systemp = runtime->plant->pdata.set.def_hcircuit.outhoff_frostfree;
-			break;
-		default:
-			err = true;
-			break;
-	}
-	pthread_rwlock_unlock(&runtime->runtime_rwlock);
-
-	if (err)
-		return false;
-
-	temp = temp_to_celsius(systemp);
-
-	dbus_rwchcd_control_complete_config_outhoff_mode_get(object, invocation, temp);
-
-	return true;
-}
-
-/**
- * D-Bus method ConfigOuthoffModeSet handler.
- * Sets the desired config default circuit outhoff temp for the given runmode.
- * @param Runmode target runmode for the new temp
- * @param NewTemp new temperature value
- * @note only handles default circuit outhoff temp for now.
- * @warning doesn't save runtime after set
- * @deprecated XXX QUICK HACK incompatible with file-based configuration.
- */
-__attribute__ ((deprecated))
-static gboolean on_handle_config_outhoff_mode_set(dbusRwchcdControl *object,
-					       GDBusMethodInvocation *invocation,
-					       guchar Runmode,
-					       gdouble NewTemp,
-					       __attribute__((unused)) gpointer user_data)
-{
-	struct s_runtime * restrict const runtime = runtime_get();
-	temp_t newtemp = celsius_to_temp(NewTemp);
-	enum e_runmode runmode = Runmode;
-	bool err = false;
-
-	if (validate_temp(newtemp) != ALL_OK)
-		return false;
-
-	pthread_rwlock_wrlock(&runtime->runtime_rwlock);
-	switch (runmode) {
-		case RM_COMFORT:
-			runtime->plant->pdata.set.def_hcircuit.outhoff_comfort = newtemp;
-			break;
-		case RM_ECO:
-			runtime->plant->pdata.set.def_hcircuit.outhoff_eco = newtemp;
-			break;
-		case RM_FROSTFREE:
-			runtime->plant->pdata.set.def_hcircuit.outhoff_frostfree = newtemp;
-			break;
-		default:
-			err = true;
-			break;
-	}
-	pthread_rwlock_unlock(&runtime->runtime_rwlock);
-
-	if (err)
-		return false;
-
-	dbus_rwchcd_control_complete_config_outhoff_mode_set(object, invocation);
-
-	return true;
+#if 0
+	registration_id = g_dbus_connection_register_subtree (connection,
+							"/org/gtk/GDBus/TestSubtree/Devices",
+							&subtree_vtable,
+							G_DBUS_SUBTREE_FLAGS_NONE,
+							NULL,  /* user_data */
+							NULL,  /* user_data_free_func */
+							NULL); /* GError** */
+	g_assert (registration_id > 0);
+#endif
 }
 
 /**
@@ -281,18 +170,6 @@ static void on_name_acquired(GDBusConnection *connection,
 			     __attribute__((unused)) const gchar *name,
 			     __attribute__((unused)) gpointer user_data)
 {
-	dbusRwchcdControl *skeleton = dbus_rwchcd_control_skeleton_new();
-	g_signal_connect(skeleton, "handle-sysmode-set", G_CALLBACK(on_handle_sysmode_set), NULL);
-	g_signal_connect(skeleton, "handle-sysmode-get", G_CALLBACK(on_handle_sysmode_get), NULL);
-	g_signal_connect(skeleton, "handle-toutdoor-get", G_CALLBACK(on_handle_toutdoor_get), NULL);
-	g_signal_connect(skeleton, "handle-config-temp-mode-get", G_CALLBACK(on_handle_config_temp_mode_get), NULL);
-	g_signal_connect(skeleton, "handle-config-temp-mode-set", G_CALLBACK(on_handle_config_temp_mode_set), NULL);
-	g_signal_connect(skeleton, "handle-config-outhoff-mode-get", G_CALLBACK(on_handle_config_outhoff_mode_get), NULL);
-	g_signal_connect(skeleton, "handle-config-outhoff-mode-set", G_CALLBACK(on_handle_config_outhoff_mode_set), NULL);
-	g_dbus_interface_skeleton_export(G_DBUS_INTERFACE_SKELETON(skeleton),
-					 connection,
-					 "/org/slashdirt/rwchcd",
-					 NULL);
 }
 
 /**
@@ -322,11 +199,17 @@ int dbus_main(void)
 {
 	guint owner_id;
 	
+	dbus_introspection_data = g_dbus_node_info_new_for_xml(dbus_introspection_xml, NULL);
+	g_assert(dbus_introspection_data != NULL);
+
+	dbus_runtime_interface_info = g_dbus_node_info_lookup_interface(dbus_introspection_data, DBUS_RUNTIME_IFACE);
+	g_assert(dbus_runtime_interface_info != NULL);
+
 	// register on dbus
 	owner_id = g_bus_own_name(G_BUS_TYPE_SYSTEM,
 				  "org.slashdirt.rwchcd",
-				  G_BUS_NAME_OWNER_FLAGS_NONE,
-				  NULL,
+				  G_BUS_NAME_OWNER_FLAGS_DO_NOT_QUEUE,
+				  on_bus_acquired,
 				  on_name_acquired,
 				  on_name_lost,
 				  NULL,
