f00b4r0/rwchcd

commit 3d1ba2d47adf1551ab2954883d81f3921a17eeea

log: add MQTT backend

log/log_mqtt.c
@@ -1,0 +1,283 @@
+//
+//  log/log_mqtt.c
+//  rwchcd
+//
+//  (C) 2020 Thibaut VARENE
+//  License: GPLv2 - http://www.gnu.org/licenses/gpl-2.0.html
+//
+
+/**
+ * @file
+ * MQTT log implementation, using Mosquitto.
+ * @warning no checks are provided to avoid doing something stupid such as overlapping the MQTT hw_backend topic space.
+ */
+
+#include <assert.h>
+#include <string.h>
+#include <stdatomic.h>
+#include <stdlib.h>
+#include <mosquitto.h>
+
+#include "log_mqtt.h"
+#include "rwchcd.h"
+#include "filecfg/dump/filecfg_dump.h"
+#include "filecfg/parse/filecfg_parser.h"
+
+static struct s_log_mqtt {
+	struct {
+		const char * restrict topic_root;///< MQTT log topic root for this backend, must not end with a '/'
+		const char * restrict username;	///< MQTT broker username (optional)
+		const char * restrict password;	///< MQTT broker password (opional)
+		const char * restrict host;	///< MQTT broker host
+		int port;			///< MQTT broker port (optional, defaults to 1883)
+		int qos;			///< MQTT QoS value (0, 1 or 2; defaults to 0)
+	} set;
+	struct {
+		bool online;			///< true if backend is online
+	} run;
+	struct mosquitto * mosq;	///< libmosquitto data
+} Log_mqtt = {
+	.set.topic_root = NULL,
+	.set.username = NULL,
+	.set.password = NULL,
+	.set.host = NULL,
+	.set.port = 1883,
+	.set.qos = 0,
+};
+
+/**
+ * Bring MQTT log backend online.
+ * @return exec status
+ */
+static int log_mqtt_online(void)
+{
+	int ret;
+
+	if (!Log_mqtt.set.host || !Log_mqtt.set.topic_root)
+		return (-EMISCONFIGURED);
+
+	mosquitto_lib_init();
+
+	Log_mqtt.mosq = mosquitto_new(NULL, true, NULL);
+	if (!Log_mqtt.mosq) {
+		ret = -EOOM;
+		goto fail;
+	}
+
+	ret = mosquitto_username_pw_set(Log_mqtt.mosq, Log_mqtt.set.username, Log_mqtt.set.password);
+	if (ret) {
+		pr_err("MQTT log username/password error: \"%s\"", mosquitto_strerror(ret));
+		ret = -EGENERIC;
+		goto fail;
+	}
+
+	ret = mosquitto_connect(Log_mqtt.mosq, Log_mqtt.set.host, Log_mqtt.set.port, 60);
+	if (ret) {
+		pr_err("MQTT log connect error: \"%s\"", mosquitto_strerror(ret));
+		return (-EGENERIC);
+	}
+
+
+	// start the network background task
+	ret = mosquitto_loop_start(Log_mqtt.mosq);
+	if (ret) {
+		pr_err("MQTT log loop start failed: \"%s\"", mosquitto_strerror(ret));
+		ret = -EGENERIC;
+		goto fail;
+	}
+
+	Log_mqtt.run.online = true;
+
+	return (ALL_OK);
+
+fail:
+	mosquitto_disconnect(Log_mqtt.mosq);
+	mosquitto_destroy(Log_mqtt.mosq);
+	mosquitto_lib_cleanup();
+	return (ret);
+}
+
+
+/**
+ * Put MQTT log backend offline.
+ */
+static void log_mqtt_offline(void)
+{
+	if (!Log_mqtt.run.online)
+		return;
+
+	Log_mqtt.run.online = false;
+
+	mosquitto_disconnect(Log_mqtt.mosq);
+	mosquitto_loop_stop(Log_mqtt.mosq, true);	// force, possibly due to https://github.com/eclipse/mosquitto/issues/1555
+
+	mosquitto_destroy(Log_mqtt.mosq);
+
+	mosquitto_lib_cleanup();
+
+	free((void *)Log_mqtt.set.host);
+	free((void *)Log_mqtt.set.username);
+	free((void *)Log_mqtt.set.password);
+	free((void *)Log_mqtt.set.topic_root);
+}
+
+/**
+ * Create the MQTT log database. NOP.
+ * @param async true if called asynchronously
+ * @param identifier the database identifier
+ * @param log_data the data to be logged
+ * @return exec status
+ */
+static int log_mqtt_create(const bool async __attribute__((unused)), const char * restrict const identifier __attribute__((unused)), const struct s_log_data * const log_data __attribute__((unused)))
+{
+	if (!Log_mqtt.run.online)
+		return (-EOFFLINE);
+
+	return (ALL_OK);
+}
+
+/**
+ * Update the MQTT log database.
+ * @param async true if called asynchronously
+ * @param identifier the database identifier
+ * @param log_data the data to be logged
+ * @return exec status
+ */
+static int log_mqtt_update(const bool async, const char * restrict const identifier, const struct s_log_data * const log_data)
+{
+	static char message[16];	// log_value_t is int32, so 10 digits + sign + '\0'. 16 is enough
+	char * topic = NULL, *p;
+	size_t basesize;
+	int msize, ret;
+	unsigned int i;
+
+	assert(identifier && log_data);
+
+	if (!Log_mqtt.run.online)
+		return (-EOFFLINE);
+
+	snprintf_automalloc(topic, basesize, "%s/%s/", Log_mqtt.set.topic_root, identifier);
+	if (!topic)
+		return (-EOOM);
+
+	// basesize accounts for terminating '\0'
+
+	for (i = 0; i < log_data->nvalues; i++) {
+		p = topic;	// save current in case realloc fails (poor man's reallocf() implementation)
+		topic = realloc(topic, basesize + strlen(log_data->keys[i]));
+		if (!topic) {
+			ret = -EOOM;
+			topic = p;
+			goto cleanup;
+		}
+		strcpy(topic + (basesize - 1), log_data->keys[i]);	// append leaf topic
+
+		msize = snprintf(message, 16, "%d", log_data->values[i]);
+		assert(msize < 16);
+
+		ret = mosquitto_publish(Log_mqtt.mosq, NULL, topic, msize, message, Log_mqtt.set.qos, false);
+		if (ret) {
+			dbgerr("mosquitto_publish failed: \"%s\"", mosquitto_strerror(ret));
+			ret = -ESTORE;
+			goto cleanup;
+		}
+	}
+
+	ret = ALL_OK;
+
+cleanup:
+	free(topic);
+	return (ret);
+}
+
+void log_mqtt_hook(struct s_log_bendcbs * restrict const callbacks)
+{
+	assert(callbacks);
+
+	callbacks->bkid = LOG_BKEND_MQTT;
+	callbacks->unversioned = true;
+	callbacks->separator = '/';
+	callbacks->log_online = log_mqtt_online;
+	callbacks->log_offline = log_mqtt_offline;
+	callbacks->log_create = log_mqtt_create;
+	callbacks->log_update = log_mqtt_update;
+}
+
+void log_mqtt_filecfg_dump(void)
+{
+	if (!Log_mqtt.run.online)
+		return;
+
+	filecfg_dump_nodestr("topic_root", Log_mqtt.set.topic_root);	// mandatory
+	filecfg_dump_nodestr("host", Log_mqtt.set.host);	// mandatory
+	filecfg_iprintf("port %d;\n", Log_mqtt.set.port);
+	filecfg_iprintf("qos %d;\n", Log_mqtt.set.qos);
+	if (FCD_Exhaustive || Log_mqtt.set.username)
+		filecfg_dump_nodestr("username", Log_mqtt.set.username ? Log_mqtt.set.username : "");	// optional
+	if (FCD_Exhaustive || Log_mqtt.set.password)
+		filecfg_dump_nodestr("password", Log_mqtt.set.password ? Log_mqtt.set.password : "");	// optional
+}
+
+FILECFG_PARSER_STR_PARSE_SET_FUNC(true, true, s_log_mqtt, topic_root)
+FILECFG_PARSER_STR_PARSE_SET_FUNC(true, true, s_log_mqtt, host)
+FILECFG_PARSER_STR_PARSE_SET_FUNC(true, true, s_log_mqtt, username)
+FILECFG_PARSER_STR_PARSE_SET_FUNC(true, true, s_log_mqtt, password)
+FILECFG_PARSER_INT_PARSE_SET_FUNC(true, s_log_mqtt, port)
+FILECFG_PARSER_INT_PARSE_SET_FUNC(true, s_log_mqtt, qos)
+
+/**
+ * Parse MQTT logging configuration.
+ * @param priv unused
+ * @param node a `backend "#LOG_BKEND_MQTT_NAME"` node
+ * @return exec status
+ */
+int log_mqtt_filecfg_parse(void * restrict const priv __attribute__((unused)), const struct s_filecfg_parser_node * const node)
+{
+	struct s_filecfg_parser_parsers parsers[] = {
+		{ NODESTR,	"topic_root",	true,	fcp_str_s_log_mqtt_topic_root,	NULL, },
+		{ NODESTR,	"host",		true,	fcp_str_s_log_mqtt_host,	NULL, },
+		{ NODESTR,	"username",	false,	fcp_str_s_log_mqtt_username,	NULL, },
+		{ NODESTR,	"password",	false,	fcp_str_s_log_mqtt_password,	NULL, },
+		{ NODEINT,	"port",		false,	fcp_int_s_log_mqtt_port,	NULL, },
+		{ NODEINT,	"qos",		false,	fcp_int_s_log_mqtt_qos,		NULL, },
+	};
+	int ret;
+
+	if ((NODESTR != node->type) || strcmp(LOG_BKEND_MQTT_NAME, node->value.stringval) || (!node->children))
+		return (-EINVALID);	// we only accept NODESTR node with children
+
+	ret = filecfg_parser_match_nodechildren(node, parsers, ARRAY_SIZE(parsers));
+	if (ALL_OK != ret)
+		return (ret);
+
+	ret = filecfg_parser_run_parsers(&Log_mqtt, parsers, ARRAY_SIZE(parsers));
+	if (ALL_OK != ret)
+		goto fail;
+
+	// minor sanity checks
+	if (!Log_mqtt.set.host || !Log_mqtt.set.topic_root) {
+		filecfg_parser_pr_err(_("In node \"%s\" closing at line %d: missing host or topic_root"), node->name, node->lineno);
+		ret = -EMISCONFIGURED;
+		goto fail;
+	}
+	else if ('/' == Log_mqtt.set.topic_root[strlen(Log_mqtt.set.topic_root)-1])
+		filecfg_parser_pr_err(_("In node \"%s\" closing at line %d: extraneous ending '/' in topic_root"), node->name, node->lineno);
+		ret = -EMISCONFIGURED;
+		goto fail;
+	}
+
+	return (ALL_OK);
+
+fail:
+	if (Log_mqtt.set.topic_root)
+		free((void *)Log_mqtt.set.topic_root);
+	if (Log_mqtt.set.host)
+		free((void *)Log_mqtt.set.host);
+	if (Log_mqtt.set.username)
+		free((void *)Log_mqtt.set.username);
+	if (Log_mqtt.set.password)
+		free((void *)Log_mqtt.set.password);
+
+	return (ret);
+}
+

filecfg/dump/log_dump.c
@@ -20,6 +20,9 @@
  #include "log/log_rrd.h"
 #endif
 #include "log/log_statsd.h"
+#ifdef HAS_MQTT
+ #include "log/log_mqtt.h"
+#endif
 
 extern struct s_log Log;
 
@@ -34,6 +37,10 @@ static const char * log_config_dump_bkend_name(const struct s_log_bendcbs * rest
 #endif
 		case LOG_BKEND_STATSD:
 			return (LOG_BKEND_STATSD_NAME);
+#ifdef HAS_MQTT
+		case LOG_BKEND_MQTT:
+			return (LOG_BKEND_MQTT_NAME);
+#endif
 		default:
 			return ("unknown");
 	}

filecfg/parse/log_parse.c
@@ -23,6 +23,9 @@
  #include "log/log_rrd.h"
 #endif
 #include "log/log_statsd.h"
+#ifdef HAS_MQTT
+ #include "log/log_mqtt.h"
+#endif
 
 extern struct s_log Log;
 
@@ -38,6 +41,9 @@ static struct {
 	{ LOG_BKEND_RRD_NAME,		log_rrd_hook,		NULL,				NULL,				false,	},	// rrd has no configuration
 #endif
 	{ LOG_BKEND_STATSD_NAME,	log_statsd_hook,	log_statsd_filecfg_parse,	log_statsd_filecfg_dump,	false,	},
+#ifdef HAS_MQTT
+	{ LOG_BKEND_MQTT_NAME,		log_mqtt_hook,		log_mqtt_filecfg_parse,		log_mqtt_filecfg_dump,		false,	},
+#endif
 };
 
 

log/Makefile
@@ -4,4 +4,8 @@ ifneq (,$(findstring HAS_RRD,$(CONFIG)))
 SRCS += log_rrd.c
 endif
 
+ifneq (,$(findstring HAS_MQTT,$(CONFIG)))
+SRCS += log_mqtt.c
+endif
+
 include $(SRCROOT)/Makefile.sub

log/log.h
@@ -32,6 +32,7 @@ enum e_log_bend {
 	LOG_BKEND_FILE,
 	LOG_BKEND_RRD,
 	LOG_BKEND_STATSD,	///< StatsD backend
+	LOG_BKEND_MQTT,		///< MQTT backend
 };
 
 /** discrete logging schedules */

log/log_mqtt.h
@@ -1,0 +1,26 @@
+//
+//  log/log_mqtt.h
+//  rwchcd
+//
+//  (C) 2020 Thibaut VARENE
+//  License: GPLv2 - http://www.gnu.org/licenses/gpl-2.0.html
+//
+
+/**
+ * @file
+ * MQTT log API.
+ */
+
+#ifndef log_mqtt_h
+#define log_mqtt_h
+
+#include "log.h"
+#include "filecfg/parse/filecfg_parser.h"
+
+#define LOG_BKEND_MQTT_NAME	"mqtt"
+
+void log_mqtt_hook(struct s_log_bendcbs * restrict const callbacks);
+void log_mqtt_filecfg_dump(void);
+int log_mqtt_filecfg_parse(void * restrict const priv, const struct s_filecfg_parser_node * const node);
+
+#endif /* log_mqtt_h */

rwchcd.xcodeproj/project.pbxproj
@@ -36,6 +36,7 @@
 		4F42A5DF1F96AD0A001B0A1F /* boiler.c in Sources */ = {isa = PBXBuildFile; fileRef = 4F42A5DD1F96AD0A001B0A1F /* boiler.c */; };
 		4F43569C23157AE60030EB89 /* hw_lib.c in Sources */ = {isa = PBXBuildFile; fileRef = 4F43569B23157AE60030EB89 /* hw_lib.c */; };
 		4F4D5C0F252A3A91003130BC /* Makefile in Sources */ = {isa = PBXBuildFile; fileRef = 4F4D5C0E252A3A91003130BC /* Makefile */; };
+		4F4E98D9254458200052EEB8 /* log_mqtt.c in Sources */ = {isa = PBXBuildFile; fileRef = 4F4E98D8254458200052EEB8 /* log_mqtt.c */; };
 		4F650A0025261AB90071EC45 /* backend.c in Sources */ = {isa = PBXBuildFile; fileRef = 4F6509FF25261AB90071EC45 /* backend.c */; };
 		4F650A09252626580071EC45 /* filecfg.c in Sources */ = {isa = PBXBuildFile; fileRef = 4F650A08252626580071EC45 /* filecfg.c */; };
 		4F869E99233A5EF300541A07 /* log_statsd.c in Sources */ = {isa = PBXBuildFile; fileRef = 4F42E98A23393156000A268A /* log_statsd.c */; };
@@ -158,6 +159,8 @@
 		4F43569A23157AE60030EB89 /* hw_lib.h */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.c.h; path = hw_lib.h; sourceTree = "<group>"; };
 		4F43569B23157AE60030EB89 /* hw_lib.c */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.c.c; path = hw_lib.c; sourceTree = "<group>"; };
 		4F4D5C0E252A3A91003130BC /* Makefile */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.make; path = Makefile; sourceTree = "<group>"; };
+		4F4E98D7254458200052EEB8 /* log_mqtt.h */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.c.h; path = log_mqtt.h; sourceTree = "<group>"; };
+		4F4E98D8254458200052EEB8 /* log_mqtt.c */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.c.c; path = log_mqtt.c; sourceTree = "<group>"; };
 		4F6509FE25261AB90071EC45 /* backend.h */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.c.h; path = backend.h; sourceTree = "<group>"; };
 		4F6509FF25261AB90071EC45 /* backend.c */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.c.c; path = backend.c; sourceTree = "<group>"; };
 		4F650A07252626580071EC45 /* filecfg.h */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.c.h; path = filecfg.h; sourceTree = "<group>"; };
@@ -429,6 +432,8 @@
 				4F299C8B218E1DBE009DCBC2 /* log_rrd.h */,
 				4F42E98A23393156000A268A /* log_statsd.c */,
 				4F42E98B23393156000A268A /* log_statsd.h */,
+				4F4E98D7254458200052EEB8 /* log_mqtt.h */,
+				4F4E98D8254458200052EEB8 /* log_mqtt.c */,
 			);
 			path = log;
 			sourceTree = SOURCE_ROOT;
@@ -675,6 +680,7 @@
 				4F0825522484383B0068FC22 /* models_parse.c in Sources */,
 				4F15A85E1EBF74880000507A /* alarms.c in Sources */,
 				4FC689642071773C00A3A065 /* hw_p1_lcd.c in Sources */,
+				4F4E98D9254458200052EEB8 /* log_mqtt.c in Sources */,
 				4F99FD2D208BCF3200DDAB75 /* hw_p1_filecfg.c in Sources */,
 				4F42A5D01F967BC3001B0A1F /* pump.c in Sources */,
 				4F01C2AD25220A74008AA53D /* temperature.c in Sources */,

commit ed53663c9a8237a3daefa3c75166162b54f0173f

log_mqtt: fix bug (uninitialized variable)
Fixes: 3d1ba2d

log/log_mqtt.c
@@ -160,17 +160,17 @@ static int log_mqtt_update(const char * restrict const identifier, const struct 
 	if (ret < 0)
 		return (-EOOM);
 
-	// basesize accounts for terminating '\0'
+	basesize = (size_t)ret;	// basesize does not account for terminating '\0'
 
 	for (i = 0; i < log_data->nvalues; i++) {
 		p = topic;	// save current in case realloc fails (poor man's reallocf() implementation)
-		topic = realloc(topic, basesize + strlen(log_data->keys[i]));
+		topic = realloc(topic, basesize + strlen(log_data->keys[i]) + 1);
 		if (!topic) {
 			ret = -EOOM;
 			topic = p;
 			goto cleanup;
 		}
-		strcpy(topic + (basesize - 1), log_data->keys[i]);	// append leaf topic
+		strcpy(topic + basesize, log_data->keys[i]);	// append leaf topic
 
 		switch (log_data->metrics[i]) {
 			case LOG_METRIC_IGAUGE: