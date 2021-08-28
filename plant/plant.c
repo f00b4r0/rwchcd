f00b4r0/rwchcd

commit 2f5a847542915d8f80b6a90dd8721dfafdfea53b

dbus: report object online status

plant/plant.c
@@ -590,7 +590,7 @@ static void plant_collect_hrequests(struct s_plant * restrict const plant)
 	// circuits first
 	for (id = 0; id < plant->hcircuits.last; id++) {
 		hcircuit = &plant->hcircuits.all[id];
-		if (!hcircuit->run.online || (ALL_OK != hcircuit->status))
+		if (!aler(&hcircuit->run.online) || (ALL_OK != hcircuit->status))
 			continue;
 
 		temp = aler(&hcircuit->run.heat_request);
@@ -610,7 +610,7 @@ static void plant_collect_hrequests(struct s_plant * restrict const plant)
 	// then dhwt
 	for (id = 0; id < plant->dhwts.last; id++) {
 		dhwt = &plant->dhwts.all[id];
-		if (!dhwt->run.online || (ALL_OK != dhwt->status))
+		if (!aler(&dhwt->run.online) || (ALL_OK != dhwt->status))
 			continue;
 
 		temp = dhwt->run.heat_request;
@@ -682,7 +682,7 @@ static void plant_dispatch_hrequests(struct s_plant * restrict const plant)
 	assert(plant->heatsources.last <= 1);	// XXX TODO: only one source supported at the moment
 	for (id = 0; id < plant->heatsources.last; id++) {
 		heatsource = &plant->heatsources.all[id];
-		if (!heatsource->run.online)
+		if (!aler(&heatsource->run.online))
 			continue;
 
 		ret = heatsource_request_temp(heatsource, plant->run.plant_hrequest);
@@ -713,7 +713,7 @@ static bool plant_summer_ok(const struct s_plant * restrict const plant)
 
 	for (id = 0; id < plant->hcircuits.last; id++) {
 		hcircuit = &plant->hcircuits.all[id];
-		if (!hcircuit->run.online)
+		if (!aler(&hcircuit->run.online))
 			continue;
 		summer &= aler(&hcircuit->set.p.bmodel->run.summer);
 	}
@@ -766,7 +766,7 @@ static int plant_summer_maintenance(struct s_plant * restrict const plant)
 	// open all valves
 	for (id = 0; id < plant->valves.last; id++) {
 		valve = &plant->valves.all[id];
-		if (!valve->run.online)
+		if (!&valve->run.online)
 			continue;
 
 		if (VA_TYPE_ISOL == valve->set.type)
@@ -784,7 +784,7 @@ static int plant_summer_maintenance(struct s_plant * restrict const plant)
 	// set all pumps ON
 	for (id = 0; id < plant->pumps.last; id++) {
 		pump = &plant->pumps.all[id];
-		if (!pump->run.online)
+		if (!aler(&pump->run.online))
 			continue;
 
 		if (pump->run.dwht_use)

dbus/dbus.c
@@ -26,6 +26,7 @@
  *
  * @note will crash if any operation is attempted before the runtime/config structures
  * are properly set, which should never happen in the current setup (dbus_main() is called after full initialization).
+ * @note the object properties are only valid if said object is 'online' (as reported by the namesake property).
  */
 
 #include <gio/gio.h>
@@ -76,6 +77,9 @@ static const gchar dbus_introspection_xml[] =
 "  </property>"
 " </interface>"
 " <interface name='" DBUS_HCIRCUIT_IFACE "'>"
+"  <property name='Online' access='read' type='b'>"
+"   <annotation name='org.freedesktop.DBus.Property.EmitsChangedSignal' value='false' />"
+"  </property>"
 "  <property name='Name' access='read' type='s'>"
 "   <annotation name='org.freedesktop.DBus.Property.EmitsChangedSignal' value='const' />"
 "  </property>"
@@ -127,6 +131,9 @@ static const gchar dbus_introspection_xml[] =
 "  <method name='DisableRunmodeOverride' />"
 " </interface>"
 " <interface name='" DBUS_DHWT_IFACE "'>"
+"  <property name='Online' access='read' type='b'>"
+"   <annotation name='org.freedesktop.DBus.Property.EmitsChangedSignal' value='false' />"
+"  </property>"
 "  <property name='Name' access='read' type='s'>"
 "   <annotation name='org.freedesktop.DBus.Property.EmitsChangedSignal' value='const' />"
 "  </property>"
@@ -172,6 +179,9 @@ static const gchar dbus_introspection_xml[] =
 "  <method name='DisableRunmodeOverride' />"
 " </interface>"
 " <interface name='" DBUS_HEATSRC_IFACE "'>"
+"  <property name='Online' access='read' type='b'>"
+"   <annotation name='org.freedesktop.DBus.Property.EmitsChangedSignal' value='false' />"
+"  </property>"
 "  <property name='Name' access='read' type='s'>"
 "   <annotation name='org.freedesktop.DBus.Property.EmitsChangedSignal' value='const' />"
 "  </property>"
@@ -186,6 +196,9 @@ static const gchar dbus_introspection_xml[] =
 "  </property>"
 " </interface>"
 " <interface name='" DBUS_PUMP_IFACE "'>"
+"  <property name='Online' access='read' type='b'>"
+"   <annotation name='org.freedesktop.DBus.Property.EmitsChangedSignal' value='false' />"
+"  </property>"
 "  <property name='Name' access='read' type='s'>"
 "   <annotation name='org.freedesktop.DBus.Property.EmitsChangedSignal' value='const' />"
 "  </property>"
@@ -202,8 +215,6 @@ static const gchar dbus_introspection_xml[] =
 "  </property>"
 " </interface>"
 "</node>";
-
-/// @todo handle offline (either report as property or report error if offline)
 
 static GMainLoop *Mainloop = NULL;
 
@@ -321,6 +332,9 @@ hcircuit_method_call(GDBusConnection       *connection,
 	if (!hcircuit)
 		goto notfound;
 
+	if (!aler(&hcircuit->run.online))
+		goto offline;
+
 	if (g_strcmp0(method_name, "SetTempOffsetOverride") == 0) {
 		gdouble offset;
 		g_variant_get(parameters, "(d)", &offset);
@@ -346,6 +360,11 @@ notfound:
 	g_dbus_method_invocation_return_dbus_error(invocation,
 						   DBUS_HCIRCUIT_IFACE ".Error.Failed",
 						   "Hcircuit not found");
+	return;
+offline:
+	g_dbus_method_invocation_return_dbus_error(invocation,
+						   DBUS_HCIRCUIT_IFACE ".Error.Failed",
+						   "Hcircuit offline");
 	return;
 invalid:
 	g_dbus_method_invocation_return_dbus_error(invocation,
@@ -386,7 +405,9 @@ hcircuit_get_property(GDBusConnection  *connection,
 
 	hcircuit = &plant->hcircuits.all[id];
 
-	if (g_strcmp0(property_name, "Name") == 0)
+	if (g_strcmp0(property_name, "Online") == 0)
+		var = g_variant_new_boolean((gboolean)aler(&hcircuit->run.online));
+	else if (g_strcmp0(property_name, "Name") == 0)
 		var = g_variant_new_string(hcircuit->name);
 	else if (g_strcmp0(property_name, "RunMode") == 0) {
 		const enum e_runmode runmode = aler(&hcircuit->overrides.o_runmode) ? aler(&hcircuit->overrides.runmode) : hcircuit->set.runmode;
@@ -480,6 +501,9 @@ dhwt_method_call(GDBusConnection       *connection,
 	if (!dhwt)
 		goto notfound;
 
+	if (!aler(&dhwt->run.online))
+		goto offline;
+
 	if (g_strcmp0(method_name, "SetRunmodeOverride") == 0) {
 		guint8 runmode;
 		g_variant_get(parameters, "(y)", &runmode);
@@ -500,6 +524,11 @@ notfound:
 	g_dbus_method_invocation_return_dbus_error(invocation,
 						   DBUS_DHWT_IFACE ".Error.Failed",
 						   "DHWT not found");
+	return;
+offline:
+	g_dbus_method_invocation_return_dbus_error(invocation,
+						   DBUS_DHWT_IFACE ".Error.Failed",
+						   "DHWT offline");
 	return;
 invalid:
 	g_dbus_method_invocation_return_dbus_error(invocation,
@@ -540,7 +569,9 @@ dhwt_get_property(GDBusConnection  *connection,
 
 	dhwt = &plant->dhwts.all[id];
 
-	if (g_strcmp0(property_name, "Name") == 0)
+	if (g_strcmp0(property_name, "Online") == 0)
+		var = g_variant_new_boolean((gboolean)aler(&dhwt->run.online));
+	else if (g_strcmp0(property_name, "Name") == 0)
 		var = g_variant_new_string(dhwt->name);
 	else if (g_strcmp0(property_name, "RunMode") == 0) {
 		const enum e_runmode runmode = aler(&dhwt->overrides.o_runmode) ? aler(&dhwt->overrides.runmode) : dhwt->set.runmode;
@@ -670,7 +701,9 @@ heatsource_get_property(GDBusConnection  *connection,
 
 	heat = &plant->heatsources.all[id];
 
-	if (g_strcmp0(property_name, "Name") == 0)
+	if (g_strcmp0(property_name, "Online") == 0)
+		var = g_variant_new_boolean((gboolean)aler(&heat->run.online));
+	else if (g_strcmp0(property_name, "Name") == 0)
 		var = g_variant_new_string(heat->name);
 	else if (g_strcmp0(property_name, "RunMode") == 0) {
 		const enum e_runmode runmode = heat->set.runmode;
@@ -732,7 +765,9 @@ pump_get_property(GDBusConnection  *connection,
 
 	pump = &plant->pumps.all[id];
 
-	if (g_strcmp0(property_name, "Name") == 0)
+	if (g_strcmp0(property_name, "Online") == 0)
+		var = g_variant_new_boolean((gboolean)aler(&pump->run.online));
+	else if (g_strcmp0(property_name, "Name") == 0)
 		var = g_variant_new_string(pump->name);
 	else if (g_strcmp0(property_name, "TurnOn") == 0)
 		var = g_variant_new_boolean((gboolean)aler(&pump->run.req_on));

plant/boiler.c
@@ -65,7 +65,7 @@ static int boiler_hs_logdata_cb(struct s_log_data * const ldata, const void * co
 	if (!boiler)
 		return (-EINVALID);
 
-	if (!hs->run.online)
+	if (!aler(&hs->run.online))
 		return (-EOFFLINE);
 
 	ldata->values[i++].i = aler(&hs->run.runmode);

plant/dhwt.c
@@ -70,7 +70,7 @@ static int dhwt_logdata_cb(struct s_log_data * const ldata, const void * const o
 	if (!dhwt)
 		return (-EINVALID);
 
-	if (!dhwt->run.online)
+	if (!aler(&dhwt->run.online))
 		return (-EOFFLINE);
 
 	ldata->values[i++].i = aler(&dhwt->run.runmode);
@@ -260,7 +260,7 @@ int dhwt_online(struct s_dhwt * const dhwt)
 	}
 
 	if (ALL_OK == ret) {
-		dhwt->run.online = true;
+		aser(&dhwt->run.online, true);
 
 		// log registration shouldn't cause onlining to fail
 		if (dhwt_log_register(dhwt) != ALL_OK)
@@ -538,7 +538,7 @@ int dhwt_run(struct s_dhwt * const dhwt)
 
 	assert(dhwt->pdata);
 
-	if (unlikely(!dhwt->run.online))	// implies set.configured == true
+	if (unlikely(!aler(&dhwt->run.online)))	// implies set.configured == true
 		return (-EOFFLINE);
 
 	ret = dhwt_logic(dhwt);

plant/dhwt.h
@@ -58,7 +58,7 @@ struct s_dhwt {
 		} p;		///< pointer-based settings. For configuration details see specific types instructions
 	} set;		///< settings (externally set)
 	struct {
-		bool online;			///< true if tank is available for use (under software management)
+		atomic_bool online;		///< true if tank is available for use (under software management)
 		bool active;			///< true if tank is active
 		atomic_bool charge_on;		///< true if charge ongoing
 		atomic_bool recycle_on;		///< true if recycle pump should be running.

plant/hcircuit.c
@@ -72,7 +72,7 @@ static int hcircuit_logdata_cb(struct s_log_data * const ldata, const void * con
 	if (!circuit)
 		return (-EINVALID);
 
-	if (!circuit->run.online)
+	if (!aler(&circuit->run.online))
 		return (-EOFFLINE);
 
 	ldata->values[i++].i = aler(&circuit->run.runmode);
@@ -282,7 +282,7 @@ int hcircuit_online(struct s_hcircuit * const circuit)
 	}
 
 	if (ALL_OK == ret) {
-		circuit->run.online = true;
+		aser(&circuit->run.online, true);
 
 		// log registration shouldn't cause onlining to fail
 		if (hcircuit_log_register(circuit) != ALL_OK)
@@ -712,7 +712,7 @@ int hcircuit_run(struct s_hcircuit * const circuit)
 	if (unlikely(!circuit))
 		return (-EINVALID);
 
-	if (unlikely(!circuit->run.online))	// implies set.configured == true
+	if (unlikely(!aler(&circuit->run.online)))	// implies set.configured == true
 		return (-EOFFLINE);
 
 	// safety checks

plant/hcircuit.h
@@ -66,7 +66,7 @@ struct s_hcircuit {
 		} p;		///< pointer-based settings. For configuration details see specific types instructions
 	} set;		///< settings (externally set)
 	struct {
-		bool online;			///< true if circuit is operational (under software management)
+		atomic_bool online;		///< true if circuit is operational (under software management)
 		bool active;			///< true if circuit is active
 		bool outhoff;			///< true if no heating conditions are met
 		bool floor_output;		///< true if the current output should not be reduced

plant/heatsource.c
@@ -61,7 +61,7 @@ int heatsource_online(struct s_heatsource * const heat)
 		ret = heat->cb.online(heat);
 
 	if (ALL_OK == ret) {
-		heat->run.online = true;
+		aser(&heat->run.online, true);
 
 		// log registration shouldn't cause online failure
 		if (heat->cb.log_reg) {
@@ -164,7 +164,7 @@ int heatsource_request_temp(struct s_heatsource * const heat, const temp_t req)
 	if (unlikely(!heat))
 		return (-EINVALID);
 
-	if (unlikely(!heat->run.online))
+	if (unlikely(!aler(&heat->run.online)))
 		return (-EOFFLINE);
 
 	aser(&heat->run.temp_request, req);
@@ -185,7 +185,7 @@ int heatsource_run(struct s_heatsource * const heat)
 	if (unlikely(!heat))
 		return (-EINVALID);
 
-	if (unlikely(!heat->run.online))	// implies set.configured == true
+	if (unlikely(!aler(&heat->run.online)))	// implies set.configured == true
 		return (-EOFFLINE);
 
 	ret = heatsource_logic(heat);

commit e6b362566029dfda81371890ac33d7186ace8310

plant_summer_maintenance(): fix code typo
Fixes: 2f5a847

plant/plant.c
@@ -769,7 +769,7 @@ static int plant_summer_maintenance(struct s_plant * restrict const plant)
 	// open all valves
 	for (id = 0; id < plant->valves.last; id++) {
 		valve = &plant->valves.all[id];
-		if (!&valve->run.online)
+		if (!valve->run.online)
 			continue;
 
 		if (VA_TYPE_ISOL == valve->set.type)