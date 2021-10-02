c73636572a583676b8dedb5bb4f9964197f234bc
Thibaut VARÈNE <hacks@slashdirt.org>
2021-10-02 16:50:07 +0200

    DHWT: fix electric untrip when plant wakes up
    Fixes: 51245a55f

--- plant/dhwt.c
+++ plant/dhwt.c
@@ -2,7 +2,7 @@
 //  plant/dhwt.c
 //  rwchcd
 //
-//  (C) 2017-2020 Thibaut VARENE
+//  (C) 2017-2021 Thibaut VARENE
 //  License: GPLv2 - http://www.gnu.org/licenses/gpl-2.0.html
 //
 
@@ -587,7 +587,6 @@ int dhwt_run(struct s_dhwt * const dhwt)
 	dhwt->run.active = true;
 
 	electric_mode = aler(&dhwt->run.electric_mode);
-	electric_mode &= dhwt->pdata->run.plant_could_sleep;	// electric is always off when !plant_could_sleep
 
 	// de-isolate the DHWT if necessary (when not on electric)
 	if (dhwt->set.p.valve_hwisol && !electric_mode) {
@@ -625,6 +624,7 @@ int dhwt_run(struct s_dhwt * const dhwt)
 
 	charge_on = aler(&dhwt->run.charge_on);
 	target_temp = aler(&dhwt->run.target_temp);
+	try_electric = (dhwt->pdata->run.plant_could_sleep || dhwt->pdata->run.hs_allfailed) && !dhwt->pdata->run.hs_overtemp;
 
 	// handle heat charge
 	/* NOTE we enforce sensor position, it SEEMS desirable, so that the full tank capacity is used before triggering a charge.
@@ -652,7 +652,6 @@ int dhwt_run(struct s_dhwt * const dhwt)
 		// trip condition
 		if (curr_temp < trip_temp) {
 			electric_mode = false;	// by default assume we can't do electric
-			try_electric = (dhwt->pdata->run.plant_could_sleep || dhwt->pdata->run.hs_allfailed) && !dhwt->pdata->run.hs_overtemp;
 			if (try_electric && (ALL_OK == outputs_relay_state_set(dhwt->set.rid_selfheater, ON))) {
 				// the plant is sleeping and we have a configured self heater: use it
 				electric_mode = true;
@@ -689,6 +688,10 @@ int dhwt_run(struct s_dhwt * const dhwt)
 
 		// untrip conditions
 		test = false;
+
+		// if running electric and we should not, stop and restart on water
+		if (electric_mode && !try_electric)
+			test = true;
 
 		// in non-electric mode and no anti-legionella charge (never interrupt an anti-legionella charge):
 		if (!electric_mode && !aler(&dhwt->run.legionella_on)) {
@@ -730,10 +733,6 @@ int dhwt_run(struct s_dhwt * const dhwt)
 			dhwt->run.mode_since = now;
 		}
 	}
-
-	// ensure we always turn off the self-heater as soon as we exit electric_mode
-	if (!electric_mode)
-		(void)!outputs_relay_state_set(dhwt->set.rid_selfheater, OFF);
 
 	// handle pump_feed - outside of the trigger since we need to manage inlet temp
 	if (dhwt->set.p.pump_feed) {
