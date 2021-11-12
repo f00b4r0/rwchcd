f00b4r0/rwchcd

commit 58d4eab41624871ba8867f6c398f8a99349c15bb

vpitctrl: disable jacketing - seems to cause more problems than it solves

plant/valve.c
@@ -202,35 +202,6 @@ static int v_pi_tcontrol(struct s_valve * const valve, const temp_t target_tout)
 	if (ALL_OK != ret)
 		tempin_l = tempin_h - vpriv->set.Ksmax;
 
-	/*
-	 If the current output is out of bound, adjust bounds.
-	 This can typically happen if e.g. the valve is open in full,
-	 the tid_hot sensor is set as the boiler sensor, and the boiler actual
-	 output at the water exhaust is higher than measured by the boiler sensor.
-	 Under these circumstances and without this adjustment, if target_tout is
-	 higher than tempin_h but lower than tempout, jacketting would still force
-	 the valve in full open position.
-	 On the low side, we factor in the current valve position in the adjustment
-	 computation, so that if we don't have a tid_cold sensor and tid_hot - Ksmax
-	 vastly overestimate tempin_l, we won't imediately close the valve after we
-	 started opening it. The more the valve is open, the closer tempout == tempin_l
-	 */
-	if (tempout > tempin_h)
-		tempin_h = tempout;
-	else if (tempout < tempin_l)
-		tempin_l = tempout - (vpriv->set.Ksmax * (1000 - valve->run.actual_position)) / 1000;
-
-	// jacketing for saturation
-	if (target_tout <= tempin_l) {		// check tempin_l first to prioritize valve closing (useful in case of temporary _h < _l)
-		valve_reqclose_full(valve);
-		valve->run.ctrl_ready = false;
-		return (ALL_OK);
-	} else if (target_tout >= tempin_h) {
-		valve_reqopen_full(valve);
-		valve->run.ctrl_ready = false;
-		return (ALL_OK);
-	}
-
 	// stop PI operation if inputs are (temporarily) inverted or too close (would make K==0)
 	if ((tempin_l >= tempin_h) || (tempin_h - tempin_l <= 1000)) {
 		valve->run.ctrl_ready = false;

commit da4630818ef8481a1479a0772ea15998dd527fef

valve: vpictrl: prevent I-term overflow
Disabling jacketing in 58d4eab had the side effect of allowing the
integral term computation to potentially overflow, inverting the valve
motion direction.

Jacket the calculated error value to the maximum output delta to prevent
this situation.

Fixes: 58d4eab

plant/valve.c
@@ -169,7 +169,7 @@ static int v_pi_tcontrol(struct s_valve * const valve, const temp_t target_tout)
 	const timekeep_t now = timekeep_now();
 	int_least16_t perth;
 	temp_t tempin_h, tempin_l, tempout;
-	tempdiff_t error, iterm, pterm, output, pthfl, Kp;
+	tempdiff_t error, iterm, pterm, output, pthfl, Kp, Ksmax;
 	const timekeep_t dt = now - vpriv->run.last_time;
 	int ret;
 	timekeep_t Ti;
@@ -227,6 +227,7 @@ static int v_pi_tcontrol(struct s_valve * const valve, const temp_t target_tout)
 	 with [A,B] in [0.1,0.8],[1,8],[10,80] for respectively aggressive, moderate and conservative tunings.
 	 Ki = Kp/Ti with Ti integration time. Ti = Tu
 	 */
+	Ksmax = (tempdiff_t)(tempin_h - tempin_l);
 	// Kp is UNSIGNED (positive) by construction, assert result is < INT32_MAX
 	Kp = (tempdiff_t)(vpriv->run.Kp_t * 1000 / (tempin_h - tempin_l));	// Make sure K cannot be 0 here. Kp_t is already scaled by VPI_FDEC
 	// Ti is UNSIGNED
@@ -234,6 +235,12 @@ static int v_pi_tcontrol(struct s_valve * const valve, const temp_t target_tout)
 
 	// calculate error E: (target - actual) - SIGNED
 	error = (tempdiff_t)(target_tout - tempout);		// error is unscaled
+
+	// jacket error to prevent overflow
+	if (error < -Ksmax)
+		error = -Ksmax;
+	else if (error > Ksmax)
+		error = Ksmax;
 
 	// Integral term I: (Ki * error) * sample interval - SIGNED
 	iterm = (Kp * error / (signed)Ti) * (signed)dt;		// iterm is scaled by VPI_FDEC