f00b4r0/rwchcd

commit a893def1203617feb48c1e4f4b61fb60baadb0d5

Implement DHW kill switch
Provide the ability to disable DHWT operation (force RM_FROSTFREE)
without having to split out sysmode management, in particular while
still alowing SYS_OFF and SYS_TEST to operate as a one-stop-shop.

runtime.c
@@ -368,3 +368,22 @@ enum e_runmode runtime_dhwmode(void)
 {
 	return (aler(&Runtime.run.dhwmode));
 }
+
+/**
+ * Get the global DHW kill switch state.
+ * @return current state
+ */
+bool runtime_get_stopdhw(void)
+{
+	return (aler(&Runtime.run.stopdhw));
+}
+
+/**
+ * Set the global DHW kill switch state.
+ * @return exec status
+ */
+int runtime_set_stopdhw(bool state)
+{
+	aser(&Runtime.run.stopdhw, state);
+	return (ALL_OK);
+}

dbus/dbus.c
@@ -75,6 +75,9 @@ static const gchar dbus_introspection_xml[] =
 "  <property name='DhwMode' access='readwrite' type='y'>"
 "   <annotation name='org.freedesktop.DBus.Property.EmitsChangedSignal' value='false' />"
 "  </property>"
+"  <property name='StopDhw' access='readwrite' type='b'>"
+"   <annotation name='org.freedesktop.DBus.Property.EmitsChangedSignal' value='false' />"
+"  </property>"
 " </interface>"
 " <interface name='" DBUS_HCIRCUIT_IFACE "'>"
 "  <property name='Online' access='read' type='b'>"
@@ -245,6 +248,8 @@ runtime_get_property(GDBusConnection  *connection,
 		enum e_runmode dhwmode = runtime_dhwmode();
 		var = g_variant_new_byte((guchar)dhwmode);
 	}
+	else if (g_strcmp0(property_name, "StopDhw") == 0)
+		var = g_variant_new_boolean((gboolean)runtime_get_stopdhw());
 	else
 		g_assert_not_reached();
 
@@ -281,6 +286,10 @@ runtime_set_property(GDBusConnection  *connection,
 			runtime_set_dhwmode(runmode);
 		else
 			goto error;
+	}
+	else if (g_strcmp0(property_name, "StopDhw") == 0) {
+		gboolean state = g_variant_get_boolean(value);
+		runtime_set_stopdhw(state);
 	}
 	else
 		g_assert_not_reached();

plant/dhwt.c
@@ -397,6 +397,8 @@ static int dhwt_logic(struct s_dhwt * restrict const dhwt)
 	// SYS_TEST/SYS_OFF always override
 	if ((SYS_TEST == sysmode) || (SYS_OFF == sysmode))
 		new_runmode = runtime_dhwmode();
+	else if (runtime_get_stopdhw())
+		new_runmode = RM_FROSTFREE;
 	else {
 		// handle global/local runmodes
 		new_runmode = aler(&dhwt->overrides.o_runmode) ? aler(&dhwt->overrides.runmode) : dhwt->set.runmode;

runtime.h
@@ -32,6 +32,7 @@ struct s_runtime {
 		_Atomic enum e_systemmode systemmode;	///< current operation mode
 		_Atomic enum e_runmode runmode;		///< CANNOT BE #RM_AUTO
 		_Atomic enum e_runmode dhwmode;		///< CANNOT BE #RM_AUTO or #RM_DHWONLY
+		atomic_bool stopdhw;			///< DHW kill switch: if true, all DHWTs will switch to RM_FROSTFREE
 	} run;
 	struct s_plant * restrict plant;	///< running plant
 };
@@ -49,5 +50,7 @@ void runtime_exit(void);
 enum e_systemmode runtime_systemmode(void);
 enum e_runmode runtime_runmode(void);
 enum e_runmode runtime_dhwmode(void);
+bool runtime_get_stopdhw(void);
+int runtime_set_stopdhw(bool state);
 
 #endif /* rwchcd_runtime_h */

commit efbb48de9d657e862d007d0c2ebd5a366128f096

runtime_restore(): restore DHW kill switch state
Fixes: a893def

runtime.c
@@ -90,6 +90,7 @@ static int runtime_restore(void)
 		aser(&Runtime.run.systemmode, aler(&temp_runtime.run.systemmode));
 		aser(&Runtime.run.runmode, aler(&temp_runtime.run.runmode));
 		aser(&Runtime.run.dhwmode, aler(&temp_runtime.run.dhwmode));
+		aser(&Runtime.run.stopdhw, aler(&temp_runtime.run.stopdhw));
 		pr_log(_("Runtime state restored"));
 	}
 	