f00b4r0/rwchcd

commit 6a3d46b9fb1eb9d5c04e1b84da4c3e5450b1eacd

hcircuit: implement inoff_temp
Setting to allow automatic heating turn off when temperature exceeds
inoff_temp threshold, with a 1K negative hysteresis for turn back on.

plant/hcircuit.c
@@ -2,7 +2,7 @@
 //  plant/hcircuit.c
 //  rwchcd
 //
-//  (C) 2017-2021 Thibaut VARENE
+//  (C) 2017-2022 Thibaut VARENE
 //  License: GPLv2 - http://www.gnu.org/licenses/gpl-2.0.html
 //
 
@@ -22,7 +22,7 @@
  * - Accelerated cooldown (per-runmode) and boost warmup transitions
  * - Optional circuit ambient temperature sensor
  * - Optional circuit water return temperature sensor
- * - Automatic circuit turn-off based on outdoor temperature evolution
+ * - Automatic circuit turn-off based on indoor/outdoor temperature evolution
  * - Timed cooldown at turn-off
  * - Min/max limits on circuit water temperature
  * - Logging of state and temperatures
@@ -282,6 +282,12 @@ int hcircuit_online(struct s_hcircuit * const circuit)
 		circuit->run.rorh_temp_increment = temp_expw_mavg(0, circuit->set.wtemp_rorh, HCIRCUIT_RORH_1HTAU, HCIRCUIT_RORH_DT);
 	}
 
+	// warn on unenforceable configuration
+	if (circuit->set.params.inoff_temp) {
+		if (inputs_temperature_get(circuit->set.tid_ambient, NULL) != ALL_OK)
+			pr_warn(_("\"%s\": inoff_temp set but no ambient sensor available: ignored."), circuit->name);
+	}
+
 	if (ALL_OK == ret) {
 		aser(&circuit->run.online, true);
 
@@ -350,10 +356,10 @@ int hcircuit_offline(struct s_hcircuit * const circuit)
 }
 
 /**
- * Conditions for running heating circuit.
+ * Outdoor conditions for running heating circuit.
  * The trigger temperature is the lowest of the set.outhoff_MODE and requested_ambient
  *
- * Circuit is off in @b ANY of the following conditions are met:
+ * Circuit is off if @b ANY of the following conditions are met:
  * - building model summer is true
  * - t_out > current temp_trigger
  * - t_out_mix > current temp_trigger
@@ -426,6 +432,39 @@ static void hcircuit_outhoff(struct s_hcircuit * const circuit, const enum e_run
 		    (t_out_mix < temp_trigger))
 			circuit->run.outhoff = false;
 	}
+}
+
+/**
+ * Indoor conditions for running heating circuit.
+ * Only applies when an ambient sensor is available and inoff_temp is set.
+ *
+ * Circuit is off if ambient temperature is > inoff_temp
+ * Circuit is back on if ambient temperature is < inoff_temp - 1K; or ambient sensor is unavailable
+ *
+ * State is preserved in all other cases.
+ * @param circuit the target circuit
+ */
+static void hcircuit_inoff(struct s_hcircuit * const circuit)
+{
+	temp_t temp_trigger, t_ambient;
+
+	// input sanitization performed in logic_hcircuit()
+	assert(circuit->pdata);
+
+	temp_trigger = SETorDEF(circuit->set.params.inoff_temp, circuit->pdata->set.def_hcircuit.inoff_temp);
+	if (!temp_trigger) {
+		circuit->run.inoff = false;
+		return;
+	}
+
+	if (inputs_temperature_get(circuit->set.tid_ambient, &t_ambient) == ALL_OK) {
+		if (t_ambient > temp_trigger)
+			circuit->run.inoff = true;
+		else if (t_ambient < temp_trigger - temp_to_deltaK(1))
+			circuit->run.inoff = false;
+	}
+	else
+		circuit->run.inoff = false;
 }
 
 /**
@@ -522,10 +561,11 @@ int hcircuit_logic(struct s_hcircuit * restrict const circuit)
 	// save current ambient request (needed by hcircuit_outhoff())
 	aser(&circuit->run.request_ambient, request_temp);
 
-	// Check if the circuit meets run.outhoff conditions
+	// Check if the circuit meets outoff/inoff conditions
 	hcircuit_outhoff(circuit, new_runmode);
+	hcircuit_inoff(circuit);
 	// if the circuit does meet the conditions (and frost is not in effect), turn it off: update runmode.
-	if (circuit->run.outhoff && !aler(&bmodel->run.frost))
+	if ((circuit->run.outhoff || circuit->run.inoff) && !aler(&bmodel->run.frost))
 		new_runmode = RM_OFF;
 
 	aser(&circuit->run.runmode, new_runmode);

dbus/dbus.c
@@ -125,6 +125,9 @@ static const gchar dbus_introspection_xml[] =
 "  <property name='OutOffFrostFree' access='read' type='d'>"
 "   <annotation name='org.freedesktop.DBus.Property.EmitsChangedSignal' value='const' />"
 "  </property>"
+"  <property name='InOffTemp' access='read' type='d'>"
+"   <annotation name='org.freedesktop.DBus.Property.EmitsChangedSignal' value='const' />"
+"  </property>"
 "  <method name='SetTempOffsetOverride'>"
 "   <arg name='offset' direction='in' type='d' />"
 "  </method>"
@@ -447,6 +450,10 @@ hcircuit_get_property(GDBusConnection  *connection,
 			temp = SETorDEF(hcircuit->set.params.outhoff_eco, hcircuit->pdata->set.def_hcircuit.outhoff_eco);
 		else if (g_strcmp0(property_name, "FrostFree") == 0)
 			temp = SETorDEF(hcircuit->set.params.outhoff_frostfree, hcircuit->pdata->set.def_hcircuit.outhoff_frostfree);
+		var = g_variant_new_double(temp_to_celsius(temp));
+	}
+	else if (g_strcmp0(property_name, "InOffTemp") == 0) {
+		temp = SETorDEF(hcircuit->set.params.inoff_temp, hcircuit->pdata->set.def_hcircuit.inoff_temp);
 		var = g_variant_new_double(temp_to_celsius(temp));
 	}
 	else {

filecfg/dump/hcircuit_dump.c
@@ -104,6 +104,8 @@ int filecfg_hcircuit_params_dump(const struct s_hcircuit_params * restrict const
 		filecfg_dump_celsius("outhoff_frostfree", params->outhoff_frostfree);
 	if (FCD_Exhaustive || params->outhoff_hysteresis)
 		filecfg_dump_deltaK("outhoff_hysteresis", params->outhoff_hysteresis);
+	if (FCD_Exhaustive || params->inoff_temp)
+		filecfg_dump_celsius("inoff_temp", params->inoff_temp);
 
 	if (FCD_Exhaustive || params->limit_wtmin)
 		filecfg_dump_celsius("limit_wtmin", params->limit_wtmin);

filecfg/parse/hcircuit_parse.c
@@ -61,6 +61,7 @@ FILECFG_PARSER_CELSIUS_PARSE_FUNC(false, false, s_hcircuit_params, outhoff_comfo
 FILECFG_PARSER_CELSIUS_PARSE_FUNC(false, false, s_hcircuit_params, outhoff_eco)
 FILECFG_PARSER_CELSIUS_PARSE_FUNC(false, false, s_hcircuit_params, outhoff_frostfree)
 FILECFG_PARSER_CELSIUS_PARSE_FUNC(true, true, s_hcircuit_params, outhoff_hysteresis)
+FILECFG_PARSER_CELSIUS_PARSE_FUNC(false, false, s_hcircuit_params, inoff_temp)
 FILECFG_PARSER_CELSIUS_PARSE_FUNC(false, false, s_hcircuit_params, limit_wtmin)
 FILECFG_PARSER_CELSIUS_PARSE_FUNC(false, false, s_hcircuit_params, limit_wtmax)
 FILECFG_PARSER_CELSIUS_PARSE_FUNC(false, true, s_hcircuit_params, temp_inoffset)
@@ -76,6 +77,7 @@ int filecfg_hcircuit_params_parse(void * restrict const priv, const struct s_fil
 		{ NODEFLT|NODEINT,	"outhoff_eco",		false,	fcp_temp_s_hcircuit_params_outhoff_eco,		NULL, },
 		{ NODEFLT|NODEINT,	"outhoff_frostfree",	false,	fcp_temp_s_hcircuit_params_outhoff_frostfree,	NULL, },
 		{ NODEFLT|NODEINT,	"outhoff_hysteresis",	false,	fcp_temp_s_hcircuit_params_outhoff_hysteresis,	NULL, },
+		{ NODEFLT|NODEINT,	"inoff_temp",		false,	fcp_temp_s_hcircuit_params_inoff_temp,		NULL, },
 		{ NODEFLT|NODEINT,	"limit_wtmin",		false,	fcp_temp_s_hcircuit_params_limit_wtmin,		NULL, },
 		{ NODEFLT|NODEINT,	"limit_wtmax",		false,	fcp_temp_s_hcircuit_params_limit_wtmax,		NULL, },
 		{ NODEFLT|NODEINT,	"temp_inoffset",	false,	fcp_temp_s_hcircuit_params_temp_inoffset,	NULL, },

filecfg/parse/plant_parse.c
@@ -25,6 +25,7 @@
 			 outhoff_eco 16.0;
 			 outhoff_frostfree 7.5;
 			 outhoff_hysteresis 1.0;
+ *			 inoff_temp 22.0;
 			 limit_wtmin 20.0;
 			 limit_wtmax 80.0;
 			 temp_inoffset 7.0;

plant/hcircuit_priv.h
@@ -75,7 +75,8 @@ struct s_hcircuit {
 	struct {
 		atomic_bool online;		///< true if circuit is operational (under software management)
 		bool active;			///< true if circuit is active
-		bool outhoff;			///< true if no heating conditions are met
+		bool outhoff;			///< true if outdoor no heating conditions are met
+		bool inoff;			///< true if indoor no heating conditions are met
 		bool floor_output;		///< true if the current output should not be reduced
 		_Atomic enum e_runmode runmode;	///< circuit actual (computed) runmode
 		enum { TRANS_NONE = 0, TRANS_UP, TRANS_DOWN } transition;	///< current transition underwent by the circuit

rwchcd.h
@@ -156,6 +156,7 @@ struct s_hcircuit_params {
 	temp_t outhoff_eco;		///< outdoor temp for no heating in eco mode
 	temp_t outhoff_frostfree;	///< outdoor temp for no heating in frostfree mode
 	temp_t outhoff_hysteresis;	///< hysteresis for no heating condition
+	temp_t inoff_temp;		///< maximum ambient temp above which heating is disabled for this circuit (only valid with an ambient sensor), a -1K hysteresis is applied to reenable
 	temp_t limit_wtmin;		///< minimum water pipe temp when this circuit is active (e.g. for frost protection)
 	temp_t limit_wtmax;		///< maximum allowed water pipe temp when this circuit is active. @warning MUST be locally or globally > 0C
 	temp_t temp_inoffset;		///< offset temp for heat source request. @note beware of interaction with e.g. boiler hysteresis

commit 7ffd969c37360832cafa0c41ebbca9c32ae97766

hcircuit_inoff(): fix hysteresis
Use correct conversion.
Fixes: 6a3d46b

plant/hcircuit.c
@@ -465,7 +465,7 @@ static void hcircuit_inoff(struct s_hcircuit * const circuit)
 	if (inputs_temperature_get(circuit->set.tid_ambient, &t_ambient) == ALL_OK) {
 		if (t_ambient > temp_trigger)
 			circuit->run.inoff = true;
-		else if (t_ambient < temp_trigger - temp_to_deltaK(1))
+		else if (t_ambient < temp_trigger - deltaK_to_temp(1))
 			circuit->run.inoff = false;
 	}
 	else